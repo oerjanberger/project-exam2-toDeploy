c47762b96367d0018a385aa02ce931ac36289723
oerjanberger <oerjan4@gmail.com>
2022-11-29 12:56:19 +0100

    Updated with fixes for reaction btn and follow btn

--- src/components/layout/ReactionBtn.js
+++ src/components/layout/ReactionBtn.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import EmojiModal from "../modals/EmojiModal";
+import { BsFillEmojiSmileFill } from "react-icons/bs";
 
 export default function ReactionBtn() {
     const [showEmojis, setShowEmojis] = useState(false);
@@ -8,6 +9,7 @@ export default function ReactionBtn() {
         <>
             <div className="specificPost__interactBtn" onClick={() => setShowEmojis(true)} >
                 <img src="/images/logo//logo_main.png" alt="react logo, react to the post" className="logo__btn" />
+                <BsFillEmojiSmileFill className="reactBtn__emoji" />
             </div>
             <EmojiModal
                 show={showEmojis}
